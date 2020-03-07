ee732aa5ce060315849f7556f9dd9d303933c8b7
GuilhemBarboux <guilhembarboux@gmail.com>
2020-03-07 14:37:02 +0100

    Feat(Trap): force rigibody kinematic on trap item

--- Assets/Scripts/Objects/Trap.cs
+++ Assets/Scripts/Objects/Trap.cs
@@ -4,14 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
 public class Trap : MonoBehaviour
 {
     private BoxCollider _collider;
+    private Rigidbody _rigidbody;
     
     private void Awake()
     {
         _collider = GetComponent<BoxCollider>();
         _collider.isTrigger = true;
+        
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
     }
 
     private void OnTriggerEnter(Collider other)

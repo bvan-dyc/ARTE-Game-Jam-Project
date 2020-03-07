0dba0c409b9cedfac3e98679573761794caa33d6
Blaise Van Dyck <blaise.vandyck@gmail.com>
2020-03-07 20:48:18 +0100

    Add Statues Trap

---
+++ Assets/Scripts/Objects/Statues.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statues : MonoBehaviour
+{
+    [SerializeField] protected List<FacingTrigger> statues = new List<FacingTrigger>();
+    protected bool cleared = false;
+
+    public bool IsCleared
+    {
+        get
+        {
+            return (cleared);
+        }
+    }
+
+    private void Update()
+    {
+        cleared = CheckStatues();
+    }
+
+    private bool CheckStatues()
+    {
+        foreach (FacingTrigger statue in statues)
+        {
+            int activatedStatues = 0;
+            if (statue.IsActivated)
+                activatedStatues++;
+            return (activatedStatues == statues.Count);
+        }
+        return false;
+    }
+}

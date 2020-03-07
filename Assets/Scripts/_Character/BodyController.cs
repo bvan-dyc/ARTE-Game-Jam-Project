b27df35f59f74049eccfd6e606e6ccf26d4b5a00
Blaise Van Dyck <blaise.vandyck@gmail.com>
2020-03-07 18:20:41 +0100

    Add animation handling

--- Assets/Scripts/_Character/BodyController.cs
+++ Assets/Scripts/_Character/BodyController.cs
@@ -10,10 +10,18 @@ public class BodyController : MonoBehaviour
     protected Animator animator;
     protected NavMeshAgent agent;
 
+    readonly int hashForwardSpeed = Animator.StringToHash("Speed");
+    readonly int hashPossessed = Animator.StringToHash("Possessed");
+
     void Awake()
     {
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void FixedUpdate()
+    {
+        animator.SetFloat(hashForwardSpeed, agent.velocity.magnitude);
     }
 
     public void ResetTarget()
@@ -27,15 +35,21 @@ public class BodyController : MonoBehaviour
         ResetTarget();
     }
 
+    public void LeaveBody()
+    {
+        ResetTarget();
+        animator.SetBool(hashPossessed, false);
+    }
+
+    public void EnterBody()
+    {
+        ResetTarget();
+        animator.SetBool(hashPossessed, true);
+    }
+
     private void PlayAudio()
     {
 
-    }
-
-    public void OnDie()
-    {
-        agent.isStopped = true;
-        agent.ResetPath();
     }
 
     public void MoveTo(Vector3 newDestination)

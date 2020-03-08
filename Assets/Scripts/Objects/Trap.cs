382bb2dea611ace95a39a41493207e11f9e9dea7
GuilhemBarboux <guilhembarboux@gmail.com>
2020-03-08 13:51:05 +0100

    WIP

--- Assets/Scripts/Objects/Trap.cs
+++ Assets/Scripts/Objects/Trap.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
-[RequireComponent(typeof(Rigidbody))]
 public class Trap : MonoBehaviour
 {
     private BoxCollider _collider;

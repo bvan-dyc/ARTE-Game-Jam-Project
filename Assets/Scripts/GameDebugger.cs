d1353a0922578e90c19855a158f67e221c55b397
Blaise Van Dyck <blaise.vandyck@gmail.com>
2020-03-08 12:36:03 +0100

    Add Debug Controls

--- Assets/Scripts/GameDebugger.cs
+++ Assets/Scripts/GameDebugger.cs
@@ -5,14 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class GameDebugger : MonoBehaviour
 {
+    [SerializeField] private bool playerDebugger = true;
     [SerializeField] private bool die = false;
 
     void Update()
     {
-        if (die)
+        if (Input.GetKeyDown(KeyCode.R))
+			Reload();
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+        if (playerDebugger && die)
         {
             PlayerController.instance.OnDie();
             die = false;
         }
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(0);
     }
 }

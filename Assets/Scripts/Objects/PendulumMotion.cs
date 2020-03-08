26c343e0de91ff616cdaa4f9d275632798a85d2d
Blaise Van Dyck <blaise.vandyck@gmail.com>
2020-03-08 15:27:32 +0100

    Add Pendulum Basics

--- Assets/Scripts/Objects/PendulumMotion.cs
+++ Assets/Scripts/Objects/PendulumMotion.cs
@@ -2,20 +2,31 @@
 
 public class PendulumMotion : MonoBehaviour
 {
-	public Quaternion RotateFrom = Quaternion.identity;
-	public Quaternion RotateTo = Quaternion.identity;
-	public float Speed = 0.0f;
+	[SerializeField] protected Quaternion RotateFrom = Quaternion.identity;
+	[SerializeField] protected Quaternion RotateTo = Quaternion.identity;
+	[SerializeField] protected float Speed = 0.0f;
+	[SerializeField] protected bool launchOnStart = false;
+	[SerializeField] protected bool moveOnlyOnce = false;
+	protected bool launch = false;
 
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
+		launch = launchOnStart;
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		var currentTime = Mathf.SmoothStep (0.0f, 1.0f, Mathf.PingPong (Time.time * Speed, 1.0f));
 
-		transform.rotation = Quaternion.Slerp (RotateFrom, RotateTo, currentTime);
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (!launch)
+			return;
+		var currentTime = Mathf.SmoothStep(0.0f, 1.0f, Mathf.PingPong(Time.time * Speed, 1.0f));
+
+		transform.rotation = Quaternion.Slerp(RotateFrom, RotateTo, currentTime);
+	}
+
+	public void LaunchPendulum()
+	{
+		launch = true;
 	}
 }
